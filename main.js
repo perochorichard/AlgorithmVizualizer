887fb38fe618181512026555cc2e3c5abe3c02f8
perochorichard <iamthedeveopernow@gmail.com>
2019-12-16 00:18:18 -0500

    Randomly generated array implemented

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 window.onload = function () {
-    svgWidth = 800;
+    this.generateSvg();
+}
+
+function generateSvg() {
+    svgWidth = $(window).width() * .7;
     svgHeight = 400;
     graphBottom = svgHeight * .93;
     $main = $('#main');
@@ -17,20 +21,38 @@ window.onload = function () {
     $xaxis.addClass('fill-primary');
     $svg.append($xaxis);
 
-    whiteSpace = 30;
-    var arr = [10, 21, 32, 48, 56, 18, 55, 6, 11, 20];
+    var arr = [...Array(20).keys()];
+    arr.sort(() => Math.random() - 0.5);
     var max = this.Math.max.apply(null, arr);
+    whiteSpace = (svgWidth / arr.length) / 2;
     for (var i = 0; i < arr.length; i++) {
-        itemHeight = (svgHeight * arr[i] / max) * .9;
+        itemHeight = (svgHeight * (arr[i] / max)) * .9;
+        itemWidth = (svgWidth / arr.length) - whiteSpace;
         xOffset = (svgWidth / arr.length) * i + whiteSpace / 2;
+        yoffset = graphBottom - itemHeight - 10;
+
+        $g = $('<g/>');
+        $g.attr('transform', 'translate(' + xOffset + ' ' + yoffset  + ')');
+
         $item = $('<rect/>');
-        $item.attr('width', (svgWidth / arr.length) - whiteSpace);
+        $item.attr('width', itemWidth);
         $item.attr('height', itemHeight);
-        $item.attr('transform', 'translate(' + xOffset + ' ' + (graphBottom - itemHeight - 10)  + ')');
         $item.addClass('fill-secondary');
-        $svg.append($item);
+
+        $text = $('<text/>');
+        $text.attr('x', (itemWidth / 2));
+        $text.attr('y', itemHeight - 10);
+        $text.attr('text-anchor', 'middle');
+        $text.addClass('fill-light');
+        $text.html(arr[i]);
+
+        $g.append($item);
+        $g.append($text);
+
+        $svg.append($g);
     }
 
     $main.html($main.html()); // refresh for svg
 }
 
+// <polyline points="20 30, 40 10, 60 30" style="fill:none;stroke:black;stroke-width:8" />
