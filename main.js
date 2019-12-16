e66fb352b3a8a46642999aff71c6d5b6ecc8bf9c
perochorichard <iamthedeveopernow@gmail.com>
2019-12-15 20:44:50 -0500

    Added svg chart

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
-window.onload = function() {
-    $slider = $('#arraySlider');
-    $('#debug').html('size: ' + $slider.val());
-    $div = $('<div/>');
-    $div.html('hello');
-    $slider.on('input', function() {
-        var size = $slider.val();
-        $('#debug').html('size: ' + size);
-        $('#main').append($div.clone());
-    })
+window.onload = function () {
+    svgWidth = 800;
+    svgHeight = 400;
+    $main = $('#main');
 
+    $svg = $('<svg/>');
+    $svg.addClass('bg-light rounded');
+    $svg.attr('width', svgWidth);
+    $svg.attr('height', svgHeight);
+    $main.append($svg);
+
+    len = 10;
+    whiteSpace = 5;
+    for (var i = 0; i < len; i++) {
+        xOffset = (svgWidth / len) * i;
+        $item = $('<rect/>');
+        $item.attr('width', (svgWidth / len) - whiteSpace);
+        $item.attr('height', svgHeight * .5);
+        $item.attr('transform', 'translate(' + xOffset + ' ' +  svgHeight*.4 + ')');
+        $item.addClass('unsorted');
+        $svg.append($item);
+    }
+
+    $main.html($main.html());
 }
