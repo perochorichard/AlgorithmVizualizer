c48ad995fe5b09e33890dbbc95fe9b8ca29689a2
perochorichard <iamthedeveopernow@gmail.com>
2019-12-16 01:08:39 -0500

    Changed all var to let

--- SvgVisualizer.js
+++ SvgVisualizer.js
@@ -7,15 +7,15 @@ export default class SvgAlgoVisualizer {
     }
 
     generateSvg(arr) {
-        var graphBottom = this.svgHeight * .9;
+        let graphBottom = this.svgHeight * .9;
     
-        var $svg = $('<svg/>');
+        let $svg = $('<svg/>');
         //$svg.addClass('bg-light');
         $svg.attr('width', this.svgWidth);
         $svg.attr('height', this.svgHeight);
     
         // create x axis
-        var $xaxis = $('<rect/>');
+        let $xaxis = $('<rect/>');
         $xaxis.attr('width', this.svgWidth);
         $xaxis.attr('height', 15);
         $xaxis.attr('transform', 'translate(0 ' + graphBottom + ')');
@@ -23,26 +23,26 @@ export default class SvgAlgoVisualizer {
         $svg.append($xaxis);
     
         // create data points
-        var max = Math.max.apply(null, arr);
+        let max = Math.max.apply(null, arr);
         let whiteSpace = (this.svgWidth / arr.length) / 2;
 
-        for (var i = 0; i < arr.length; i++) {
+        for (let i = 0; i < arr.length; i++) {
             let itemHeight = (this.svgHeight * (arr[i] / max)) * .87;
             let itemWidth = (this.svgWidth / arr.length) - whiteSpace;
             let itemXOffset = (this.svgWidth / arr.length) * i + whiteSpace / 2;
             let itemYoffset = graphBottom - itemHeight - 10;
 
-            var $g = $('<g/>');
+            let $g = $('<g/>');
             $g.attr('transform', 'translate(' + itemXOffset + ' ' + itemYoffset + ')');
     
-            var $item = $('<rect/>');
+            let $item = $('<rect/>');
             $item.attr('width', itemWidth);
             $item.attr('height', itemHeight);
             $item.addClass('fill-secondary');
             $g.append($item);
     
             if (arr.length <= 20) {
-                var $text = $('<text/>');
+                let $text = $('<text/>');
                 $text.attr('x', (itemWidth / 2));
                 $text.attr('y', itemHeight - 10);
                 $text.attr('text-anchor', 'middle');
--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 import SvgVisualizer from './SvgVisualizer.js';
+
 window.onload = function() {
-    var width = window.innerWidth * 0.8;
-    var height = 400;
-    var svg = new SvgVisualizer(height, width);
+    let width = window.innerWidth * 0.8;
+    let height = 400;
+    let svg = new SvgVisualizer(height, width);
     $('#main').append(svg.generateSvg([...Array(20).keys()].map(i => ++i).sort(() => Math.random() - 0.5)));
     $('#main').html($('#main').html());
-    //        var arr = [...Array(len).keys()].map(x => ++x);
-    //arr.sort(() => Math.random() - 0.5);
 }
