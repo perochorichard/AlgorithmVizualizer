186b73af0e617b2fc57e993b85d248bcb846c2ce
perochorichard <iamthedeveopernow@gmail.com>
2019-12-16 22:48:44 -0500

    Changed graph from svg to table

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
-import SvgVisualizer from './SvgVisualizer.js';
 
 window.onload = function() {
-    let width = window.innerWidth * 0.8;
-    let height = 400;
-    let svg = new SvgVisualizer(height, width);
-    $('#main').append(svg.generateSvg([...Array(20).keys()].map(i => ++i).sort(() => Math.random() - 0.5)));
-    $('#main').html($('#main').html());
+    let arr = [...Array(20).keys()].map(i => ++i).sort(() => Math.random() - 0.5);
+    let max = Math.max.apply(null, arr);
+    let maxHeight = 200;
+
+    let $dataPoints = $('#datapoints');
+    let $dPointContainer = $('<td/>');
+    $dPointContainer.addClass('align-bottom');
+    let $dPoint = $('<div/>');
+    $dPoint.addClass('bg-unsorted');
+
+    for (var i = 0; i < arr.length; i++) {
+        let height = maxHeight * (arr[i] / max);
+
+        let dpoint = $dPointContainer.clone().append(
+            $dPoint.clone().css('height', (height + 'px'))
+        );
+        $dataPoints.append(dpoint);
+    }
 }
