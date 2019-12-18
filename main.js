0a5c3d7cbb0f4b6f0df3443f20ccb7160a4d619f
perochorichard <iamthedevelopernow@gmail.com>
2019-12-18 00:51:10 -0500

    Added swap function

--- main.js
+++ main.js
@@ -1,24 +1,42 @@
 
 window.onload = function () {
-    generateDataPoints(5);
+    let $tbody = $('#tablebody');
+    let data = generateDataPoints(5);
+    $tbody.append(data);
+
+    $('#xaxis').attr('colspan', 200).append(
+        $('<div/>').css('height',  30)
+            .addClass('bg-info mt-1'));
 
     $(document).on('input', '#dataArrayRange', function () {
+        data = generateDataPoints(parseInt($(this).val()), 10);
+        $tbody.children().remove('#datapoints');
+        $tbody.append(data);
 
     });
+
+    $('#sort').on('click', function() {
+        let first = data.children().eq(1).children().first();
+        let second = data.children().eq(3).children().first();
+
+        swap(first, second);
+    });
+}
+
+function swap(a, b) {
+    let aHeight = a.css('height');
+    a.css('height', b.css('height'));
+    b.css('height', aHeight);
 }
 
 function generateDataPoints(len) {
     let arr = [...Array(len).keys()].map(i => ++i).sort(() => Math.random() - 0.5);
     let max = Math.max.apply(null, arr);
-    let maxHeight = 200;
+    let maxHeight = 500;
     let width = 500 * (1 / arr.length);
 
-    $('#xaxis').attr('colspan', arr.length).append(
-        $('<div/>').css('height', (width > 30) ? 30 : width)
-            .addClass('bg-info mt-1')
-    );
-
-    let $dataPoints = $('#datapoints');
+    let $dataPoints = $('<tr/>');
+    $dataPoints.attr('id', 'datapoints');
     let $dPointContainer = $('<td/>');
     $dPointContainer.addClass('align-bottom p-0');
     let $dPoint = $('<div/>');
@@ -35,4 +53,6 @@ function generateDataPoints(len) {
         );
         $dataPoints.append(dpoint);
     }
+
+    return $dataPoints;
 }
